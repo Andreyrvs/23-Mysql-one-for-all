13fe564ad4568fc25c2e594d99ae0d9da2489083
Andreyrvs <cortaesai@gmail.com>
2022-06-07 10:48:36 -0300

    Fix: desafio 01 - muda o .json e muda relacionamento das tabelas de album, cancoes

--- desafio1.sql
+++ desafio1.sql
@@ -55,6 +55,7 @@ VALUES
     (6,	"Fog");
 
 CREATE TABLE SpotifyClone.usuario_segue_artista (
+	id INT PRIMARY KEY NOT NULL,
     usuario_id INT,
     artista_id INT,
     FOREIGN KEY (usuario_id)
@@ -64,224 +65,155 @@ CREATE TABLE SpotifyClone.usuario_segue_artista (
 )  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.usuario_segue_artista
-(usuario_id, artista_id)
+(id, usuario_id, artista_id)
 VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(2, 1),
-(2, 3),
-(3, 4),
-(3, 1),
-(4, 2),
-(5, 5),
-(5, 6),
-(6, 6),
-(6, 3),
-(6, 1),
-(7, 4),
-(7, 5),
-(8, 1),
-(8, 5),
-(9, 6),
-(9, 2),
-(9, 3),
-(10, 4),
-(10, 6);
+(1,1,1),
+(2,1,2),
+(3,1,3),
+(4,2,1),
+(5,2,3),
+(6,3,4),
+(7,3,1),
+(8,4,2),
+(9,5,5),
+(10,5,6),
+(11,6,6),
+(12,6,3),
+(13,6,1),
+(14,7,4),
+(15,7,5),
+(16,8,1),
+(17,8,5),
+(18,9,6),
+(19,9,2),
+(20,9,3),
+(21,10,4),
+(22,10,6);
 
 CREATE TABLE SpotifyClone.album (
     album_id INT PRIMARY KEY NOT NULL,
     album VARCHAR(45),
-    ano_lancamento VARCHAR(45)
+    ano_lancamento VARCHAR(45),
+    artista_id INT,
+    FOREIGN KEY (artista_id) REFERENCES artista (artista_id)
 )  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.album
-(album_id,album,ano_lancamento)
+(album_id,album,ano_lancamento, artista_id)
 VALUES
-(1,"Envious",1990),
-(2,"Exuberant",1993),
-(3,"Hallowed Steam",1995),
-(4,"Incandescent",1998),
-(5,"Temporary Culture",2001),
-(6,"Library of liberty",2003),
-(7,"Chained Down",2007),
-(8,"Cabinet of fools",2012),
-(9,"No guarantees",2015),
-(10,"Apparatus",2015);
-
-CREATE TABLE SpotifyClone.album_artista (
-    album_id INT,
-    artista_id INT,
-    FOREIGN KEY (album_id)
-        REFERENCES album (album_id),
-    FOREIGN KEY (artista_id)
-        REFERENCES artista (artista_id)
-)  ENGINE=INNODB;
-
-INSERT INTO SpotifyClone.album_artista
-(album_id, artista_id)
-VALUES
-(1,1),
-(2,1),
-(3,4),
-(4,3),
-(5,2),
-(6,2),
-(7,5),
-(8,5),
-(9,5),
-(10,6);
+(1,"Envious",1990,1),
+(2,"Exuberant",1993,1),
+(3,"Hallowed Steam",1995,4),
+(4,"Incandescent",1998,3),
+(5,"Temporary Culture",2001,2),
+(6,"Library of liberty",2003,2),
+(7,"Chained Down",2007,5),
+(8,"Cabinet of fools",2012,5),
+(9,"No guarantees",2015,5),
+(10,"Apparatus",2015,6);
 
 CREATE TABLE SpotifyClone.cancoes (
-	cancao_id INT PRIMARY KEY NOT NULL,
+    cancao_id INT PRIMARY KEY NOT NULL,
     nome VARCHAR(45),
-    duracao_segundos INT
+    duracao_segundos INT,
+    album_id INT,
+    FOREIGN KEY (album_id)
+        REFERENCES album (album_id)
 )  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.cancoes
-(cancao_id, nome, duracao_segundos)
+(cancao_id, nome, duracao_segundos, album_id)
 VALUES
-(1,"Soul For Us",200),
-(2,"Reflections Of Magic",163),
-(3,"Dance With Her Own",116),
-(4,"Troubles Of My Inner Fire",203),
-(5,"Time Fireworks",152),
-(6,"Magic Circus",105),
-(7,"Honey, So Do I",207),
-(8,"Sweetie, Let's Go Wild",139),
-(9,"She Knows",244),
-(10,"Fantasy For Me",100),
-(11,"Celebration Of More",146),
-(12,"Rock His Everything",223),
-(13,"Home Forever",231),
-(14,"Diamond Power",241),
-(15,"Let's Be Silly",132),
-(16,"Thang Of Thunder",240),
-(17,"Words Of Her Life",185),
-(18,"Without My Streets",176),
-(19,"Need Of The Evening",190),
-(20,"History Of My Roses",222),
-(21,"Without My Love",111),
-(22,"Walking And Game",123),
-(23,"Young And Father",197),
-(24,"Finding My Traditions",179),
-(25,"Walking And Man",229),
-(26,"Hard And Time",135),
-(27,"Honey, I'm A Lone Wolf",150),
-(28,"She Thinks I Won't Stay Tonight",166),
-(29,"He Heard You're Bad For Me",154),
-(30,"He Hopes We Can't Stay",210),
-(31,"I Know I Know",117),
-(32,"He's Walking Away",159),
-(33,"He's Trouble",138),
-(34,"I Heard I Want To Bo Alone",120),
-(35,"I Ride Alone",151),
-(36,"Honey",79),
-(37,"You Cheated On Me",95),
-(38,"Wouldn't It Be Nice",213),
-(39,"Baby",136),
-(40,"You Make Me Feel So..",83);
-
-CREATE TABLE SpotifyClone.album_cancoes (
-    album_id INT,
-    cancao_id INT,
-    FOREIGN KEY (album_id)
-        REFERENCES album (album_id),
-    FOREIGN KEY (cancao_id)
-        REFERENCES cancoes (cancao_id)
-)  ENGINE=INNODB;
-
-INSERT INTO SpotifyClone.album_cancoes
-(album_id, cancao_id)
-VALUES
-(1,1),
-(1,2),
-(1,3),
-(2,4),
-(2,5),
-(3,6),
-(3,7),
-(3,8),
-(3,9),
-(4,10),
-(4,11),
-(4,12),
-(4,13),
-(4,14),
-(4,15),
-(5,16),
-(5,17),
-(5,18),
-(6,19),
-(6,20),
-(6,21),
-(6,22),
-(6,23),
-(7,24),
-(7,25),
-(7,26),
-(7,27),
-(8,28),
-(8,29),
-(8,30),
-(8,31),
-(9,32),
-(9,33),
-(9,34),
-(9,35),
-(10,36),
-(10,37),
-(10,38),
-(10,39),
-(10,40);
+(1,"Soul For Us",200,1),
+(2,"Reflections Of Magic",163,1),
+(3,"Dance With Her Own",116,1),
+(4,"Troubles Of My Inner Fire",203,2),
+(5,"Time Fireworks",152,2),
+(6,"Magic Circus",105,3),
+(7,"Honey, So Do I",207,3),
+(8,"Sweetie, Let's Go Wild",139,3),
+(9,"She Knows",244,3),
+(10,"Fantasy For Me",100,4),
+(11,"Celebration Of More",146,4),
+(12,"Rock His Everything",223,4),
+(13,"Home Forever",231,4),
+(14,"Diamond Power",241,4),
+(15,"Let's Be Silly",132,4),
+(16,"Thang Of Thunder",240,5),
+(17,"Words Of Her Life",185,5),
+(18,"Without My Streets",176,5),
+(19,"Need Of The Evening",190,6),
+(20,"History Of My Roses",222,6),
+(21,"Without My Love",111,6),
+(22,"Walking And Game",123,6),
+(23,"Young And Father",197,6),
+(24,"Finding My Traditions",179,7),
+(25,"Walking And Man",229,7),
+(26,"Hard And Time",135,7),
+(27,"Honey, I'm A Lone Wolf",150,7),
+(28,"She Thinks I Won't Stay Tonight",166,8),
+(29,"He Heard You're Bad For Me",154,8),
+(30,"He Hopes We Can't Stay",210,8),
+(31,"I Know I Know",117,8),
+(32,"He's Walking Away",159,9),
+(33,"He's Trouble",138,9),
+(34,"I Heard I Want To Bo Alone",120,9),
+(35,"I Ride Alone",151,9),
+(36,"Honey",79,10),
+(37,"You Cheated On Me",95,10),
+(38,"Wouldn't It Be Nice",213,10),
+(39,"Baby",136,10),
+(40,"You Make Me Feel So..",83,10);
 
 CREATE TABLE SpotifyClone.historico_usuario (
+    id INT PRIMARY KEY NOT NULL,
     usuario_id INT,
     cancao_id INT,
     data_reproducao VARCHAR(45),
     FOREIGN KEY (usuario_id)
         REFERENCES usuario (usuario_id),
-        FOREIGN KEY (cancao_id) REFERENCES SpotifyClone.cancoes (cancao_id) 
+    FOREIGN KEY (cancao_id)
+        REFERENCES SpotifyClone.cancoes (cancao_id)
 )  ENGINE=INNODB;
 
 INSERT INTO SpotifyClone.historico_usuario
-(usuario_id, cancao_id, data_reproducao)
+(id, usuario_id, cancao_id, data_reproducao)
 VALUES
-(1,36,"2020-02-28 10:45:55"),
-(1,25, "2020-05-02 05:30:35"),
-(1,23, "2020-03-06 11:22:33"),
-(1,14, "2020-08-05 08:05:17"),
-(1,15, "2020-09-14 16:32:22"),
-(2,34,"2020-01-02 07:40:33"),
-(2,24, "2020-05-16 06:16:22"),
-(2,21, "2020-10-09 12:27:48"),
-(2,39, "2020-09-21 13:14:46"),
-(3,6,"2020-11-13 16:55:13"),
-(3,3, "2020-12-05 18:38:30"),
-(3,26, "2020-07-30 10:00:00"),
-(4,2,"2021-08-15 17:10:10"),
-(4,35, "2021-07-10 15:20:30"),
-(4,27, "2021-01-09 01:44:33"),
-(5,7,"2020-07-03 19:33:28"),
-(5,12, "2017-02-24 21:14:22"),
-(5,14, "2020-08-06 15:23:43"),
-(5,1, "2020-11-10 13:52:27"),
-(6,38,"2019-02-07 20:33:48"),
-(6,29, "2017-01-24 00:31:17"),
-(6,30, "2017-10-12 12:35:20"),
-(6,22, "2018-05-29 14:56:41"),
-(7,5,"2018-05-09 22:30:49"),
-(7,4, "2020-07-27 12:52:58"),
-(7,11, "2018-01-16 18:40:43"),
-(8,39,"2018-03-21 16:56:40"),
-(8,40, "2020-10-18 13:38:05"),
-(8,32, "2019-05-25 08:14:03"),
-(8,33, "2021-08-15 21:37:09"),
-(9,16,"2021-05-24 17:23:45"),
-(9,17, "2018-12-07 22:48:52"),
-(9,8, "2021-03-14 06:14:26"),
-(9,9, "2020-04-01 03:36:00"),
-(10,20,"2017-02-06 08:21:34"),
-(10,21, "2017-12-04 05:33:43"),
-(10,12, "2017-07-27 05:24:49"),
-(10,13, "2017-12-25 01:03:57"); 
+(1,1,36,"2020-02-28 10:45:55"),
+(2,1,25, "2020-05-02 05:30:35"),
+(3,1,23, "2020-03-06 11:22:33"),
+(4,1,14, "2020-08-05 08:05:17"),
+(5,1,15, "2020-09-14 16:32:22"),
+(6,2,34,"2020-01-02 07:40:33"),
+(7,2,24, "2020-05-16 06:16:22"),
+(8,2,21, "2020-10-09 12:27:48"),
+(9,2,39, "2020-09-21 13:14:46"),
+(10,3,6,"2020-11-13 16:55:13"),
+(11,3,3, "2020-12-05 18:38:30"),
+(12,3,26, "2020-07-30 10:00:00"),
+(13,4,2,"2021-08-15 17:10:10"),
+(14,4,35, "2021-07-10 15:20:30"),
+(15,4,27, "2021-01-09 01:44:33"),
+(16,5,7,"2020-07-03 19:33:28"),
+(17,5,12, "2017-02-24 21:14:22"),
+(18,5,14, "2020-08-06 15:23:43"),
+(19,5,1, "2020-11-10 13:52:27"),
+(20,6,38,"2019-02-07 20:33:48"),
+(21,6,29, "2017-01-24 00:31:17"),
+(22,6,30, "2017-10-12 12:35:20"),
+(23,6,22, "2018-05-29 14:56:41"),
+(24,7,5,"2018-05-09 22:30:49"),
+(25,7,4, "2020-07-27 12:52:58"),
+(26,7,11, "2018-01-16 18:40:43"),
+(27,8,39,"2018-03-21 16:56:40"),
+(28,8,40, "2020-10-18 13:38:05"),
+(29,8,32, "2019-05-25 08:14:03"),
+(30,8,33, "2021-08-15 21:37:09"),
+(31,9,16,"2021-05-24 17:23:45"),
+(32,9,17, "2018-12-07 22:48:52"),
+(33,9,8, "2021-03-14 06:14:26"),
+(34,9,9, "2020-04-01 03:36:00"),
+(35,10,20,"2017-02-06 08:21:34"),
+(36,10,21, "2017-12-04 05:33:43"),
+(37,10,12, "2017-07-27 05:24:49"),
+(38,10,13, "2017-12-25 01:03:57");
